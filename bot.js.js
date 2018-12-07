aed359fec6fc11e7d72d6116753952c5cf7388db
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-12-07 22:05:40 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -1,90 +1,11 @@
-﻿const Discord = require('discord.js');
-const client = new Discord.Client();
-const ayarlar = require('./ayarlar.json');
-const chalk =dfsdfsdfsdfsdf
-      client.aliases.set(alias, props.help.name);
-    });
-  });
-});
-
-client.reload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
-
-client.load = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.set(command, cmd);
-      cmd.conf.aliases.forEach(alias => {
-        client.aliases.set(alias, cmd.help.name);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
-
-client.unload = command => {
-  return new Promise((resolve, reject) => {
-    try {
-      delete require.cache[require.resolve(`./komutlar/${command}`)];
-      let cmd = require(`./komutlar/${command}`);
-      client.commands.delete(command);
-      client.aliases.forEach((cmd, alias) => {
-        if (cmd === command) client.aliases.delete(alias);
-      });
-      resolve();
-    } catch (e){
-      reject(e);
-    }
-  });
-};
-
-client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'velactus') {
-    msg.reply('eu.velactus.club Pek Yakinda!');
-  }
-});
-
-client.elevation = message => {
-  if(!message.guild) {
-	return; }
-  let permlvl = 0;
+﻿const Dissdg
   if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
   if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
   if (message.author.id === ayarlar.sahip) permlvl = 4;
   return permlvl;
 };
-
-var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-// client.on('debug', e => {
-//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
-// });
-
-client.on('warn', e => {
-  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
-});
-
-client.on('error', e => {
-  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+gsdt.on('debug', e => {
+//   consolsplace(regToken, 'that was redacted')));
 });
 
 client.login(process.env.BOT_TOKEN);
