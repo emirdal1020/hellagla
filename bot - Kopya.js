9c809dd064c822bc4c0f668abf6b892f969e7e43
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-12-07 22:05:57 +0300

    Update bot - Kopya.js

--- bot - Kopya.js
+++ bot - Kopya.js
@@ -1,33 +1,4 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const ayarlar = require('./ayarlar.json');
-
-var prefix = ayarlar.prefix;
-
-client.on('ready', () => {
-  console.log(`BOT: ${client.user.username} adı ile giriş yaptı!`);
-});
-
-client.on('message', msg => {
-  console.log(`LOG: S: ${msg.guild.name} M: ${msg.content} Y: ${msg.author.tag}`);
-  if (msg.author.id === ayarlar.id) return;
-  if (msg.author.bot) return;
-
-  if (!msg.content.startsWith(prefix)) {
-	  return;
-  }
-  if (msg.content.toLowerCase() === prefix + 'ping') {
-    msg.reply('Pong! **' + client.ping + '** ms');
-  }
-  if (msg.content.toLowerCase() === prefix + 'sa') {
-    msg.reply('Aleyküm selam!');
-  }
-  if (msg.content.toLowerCase() === prefix + 'yaz') {
-    msg.delete();
-    msg.channel.sendMessage(msg.content);
-  }
-  if (msg.content.toLowerCase() === prefix + 'temizle') {
-    msg.channel.bulkDelete(100);
+dgsdgscbcxv snr gsdannel.bulkDelete(100);
     msg.channel.sendMessage("100 adet mesaj silindi! (Emirhan Saraç)");
   }
   if (msg.content.toLowerCase() === prefix + 'reboot') {
