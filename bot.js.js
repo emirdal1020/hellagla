12299bc4ff10dfb5386f03a8f7f2e305edb21206
emirdal1020 <45396268+emirdal1020@users.noreply.github.com>
2018-12-07 22:05:24 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -1,27 +1,7 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
-const chalk = require('chalk');
-const fs = require('fs');
-const moment = require('moment');
-require('./util/eventLoader')(client);
-
-var prefix = ayarlar.prefix;
-
-const log = message => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
-};
-
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-fs.readdir('./komutlar/', (err, files) => {
-  if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
-  files.forEach(f => {
-    let props = require(`./komutlar/${f}`);
-    log(`Yüklenen komut: ${props.help.name}.`);
-    client.commands.set(props.help.name, props);
-    props.conf.aliases.forEach(alias => {
+const chalk =dfsdfsdfsdfsdf
       client.aliases.set(alias, props.help.name);
     });
   });
